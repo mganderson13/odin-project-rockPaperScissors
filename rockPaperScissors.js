9e879fe99d1c60f553c7e618b8bd40d270d69297
Marianne Anderson <mganderson13@gmail.com>
2024-09-12 16:22:59 -0500

    fixed computerChoice and wrote humanChoice logic

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,15 +5,42 @@ function getComputerChoice() {
 
     let outcome = ""
 
-    let chance = Math.floor(Math.random() * 100);
+    let chance = Math.floor(Math.random() * 3);
 
-    if (chance <= 33) {
+    if (chance === 0) {
         outcome = "Rock";
-    } else if (chance > 33 && chance <= 66) {
+    } else if (chance === 1) {
         outcome = "Paper";
     } else {
         outcome = "Scissors"
     }
 
+    console.log("Computer's move: ", outcome);
+
     return outcome;
 }
+
+getComputerChoice();
+
+function getHumanChoice() {
+
+   let humanChoice = parseInt(window.prompt("Please enter your move choice. For Rock enter 0. For Paper enter 1. For Scissors enter 2."));
+
+    let humanOutcome = "";
+
+    if (humanChoice === 0) {
+        humanOutcome = "Rock";
+    } else if (humanChoice === 1) {
+        humanOutcome = "Paper";
+    } else if (humanChoice === 2){
+        humanOutcome = "Scissors"
+    } else {
+        alert("Must enter either 0, 1, or 2.")
+    }
+
+    console.log("Your move: ", humanOutcome);
+
+    return humanOutcome;
+}
+
+getHumanChoice();
